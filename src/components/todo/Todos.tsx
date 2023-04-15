ee050f3cc22dfcd567351804220f331fdd02f058
pzajas <zajas.piotr@gmail.com>
2023-04-15 12:14:00 +0200

    #: In PrimaryForm component add a functionality to recive an array of inputs to render as many inputs as the user wants.

--- src/components/todo/Todos.tsx
+++ src/components/todo/Todos.tsx
@@ -1,10 +1,15 @@
 import { PrimaryForm as Form } from './PrimaryForm'
-
 import { handleSubmit } from './onSubmitFunction'
 import { useDispatch } from 'react-redux'
 
+import { type InputArrayType } from '@typescript/types'
+
 const Todos = (): JSX.Element => {
   const dispatch = useDispatch()
+
+  const inputsArray: InputArrayType[] = [
+    { type: 'text', name: 'text', placeholder: 'Add a todo' },
+  ]
 
   return (
     <div>
@@ -12,7 +17,7 @@ const Todos = (): JSX.Element => {
         onSubmit={async (data) => {
           await handleSubmit(data, dispatch)
         }}
-        inputName="text"
+        inputs={inputsArray}
       />
     </div>
   )
